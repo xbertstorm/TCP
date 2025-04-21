6ed7bed721a889e840eea93464af0df165074684
Jerry Chuang <32446925+xbertstorm@users.noreply.github.com>
2025-04-21 14:13:59 +0800

    Update 0.py

--- 0.py
+++ 0.py
@@ -1,14 +1,14 @@
+#!/usr/bin/env python
+
 import asyncio
-import websockets
+from websockets.asyncio.server import serve
 
-async def echo(websocket, path):
-    print('echo')
+async def echo(websocket):
     async for message in websocket:
-        print(message,'received from client')
-        greeting = f"Hello {message}!"
-        await websocket.send(greeting)
-        print(f"> {greeting}")
+        await websocket.send(message)
 
-asyncio.get_event_loop().run_until_complete(
-    websockets.serve(echo, '0.0.0.0', os.getenv("PORT")))
-asyncio.get_event_loop().run_forever()
+async def main():
+    async with serve(echo, "localhost", 8765) as server:
+        await server.serve_forever()
+
+asyncio.run(main())
